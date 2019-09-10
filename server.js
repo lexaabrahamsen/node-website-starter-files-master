57c36c82fb96f26fd95bf5622898e1dcf517eb58
Lexa <lexaraea@gmail.com>
2019-09-10 15:19:28 -0400

    Passing variables in Pug

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', {
+    name: 'Ayo'
+  });
 });
 
 const server = app.listen(7000, () => {
-  console.log(`Express running → PORT
-     ${server.address().port}`);
+  console.log(`Express running → PORT ${server.address().port}`);
 });
