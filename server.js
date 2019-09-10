00b7ced463c0c77a9df83479821a7922a0ceb64d
Lexa <lexaraea@gmail.com>
2019-09-10 15:27:20 -0400

    Adding people (working with JSON data

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const people = require('./people.json');
+
 const app = express();
 
 app.set('view engine', 'pug');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.render('index', {
-    name: 'Ayo'
+    title: 'Homepage',
+    people: people.profiles
   });
 });
 
